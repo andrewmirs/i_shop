8ab04bb094fd56bea949f4476b8bde3afcf76234
Sridhar Madala <Si.madala19@gmail.com>
2018-12-20 13:26:56 -0800

    Setup initial express server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { resolve } = require('path');
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// app.use(express.static());
+
+app.get('/', (req, resp) => {
+    resp.send('<h1>Server is runnin fool<h1>');
+})
+
+app.listen(PORT, () => {
+    console.log('I\'m listening on PORT: ' + PORT + ' SHHHHHHHHHHHHHHH');
+}).on('error', (err) => {
+    console.log('Listen Error: You already have a surver running on PORT: ' + PORT)
+})
