c53fe51c8a6b45255f30b6c3821aecb49a6f4542
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-12-28 11:38:02 -0800

    render auth or nonAuth links to nav bar with helper functions

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './nav.scss';
 
 class Nav extends Component {
     state = {
@@ -34,9 +36,49 @@ class Nav extends Component {
         ]
     }
 
+    renderSignOut(){
+       return <button className="btn wave-effect waves-light blue" onClick={() => console.log('Sign Out Clicked')}>Sign Out</button>
+    }
+
+    buildLink(link){
+        return (
+            <li key={link.to} >
+                <Link to={link.to}>{link.text}</Link> 
+            </li>
+        );
+    }
+
+    renderLinks(){
+        const auth = true;
+        let authLinks = [];
+
+        const { auth: navAuth, common, nonAuth } = this.state
+
+        const commonLinks = common.map(this.buildLink);
+
+        if(auth){
+            authLinks = navAuth.map(this.buildLink);
+            authLinks.push(<li key="/sign-out">{this.renderSignOut()}</li>);
+        } else {
+            authLinks = nonAuth.map(this.buildLink);
+        }
+
+        console.log(authLinks);
+
+        return [...commonLinks, ...authLinks];
+    }
+
     render() { 
         return ( 
-            <nav className="blue-grey darken-2"></nav>
+            <nav className="blue-grey darken-2">
+                <div className="nav-wrapper">
+                    <Link className="brand-logo" to="/">iShop</Link>
+
+                    <ul className="right">
+                        {this.renderLinks()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
